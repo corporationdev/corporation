913f43f9fea37ff6b8f8f130acdf36459064841f
Isaac Dyor <isaac@dyor.com>
2026-02-24 15:57:15 -0800

    fix gaurd

--- packages/app/src/routes/_public.tsx
+++ packages/app/src/routes/_public.tsx
@@ -5,7 +5,7 @@ import { authClient } from "@/lib/auth-client";
 export const Route = createFileRoute("/_public")({
 	beforeLoad: async () => {
 		const session = await authClient.getSession();
-		if (session.data) {
+		if (session.data?.user) {
 			throw redirect({ to: "/space" });
 		}
 	},
