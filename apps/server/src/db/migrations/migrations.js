ab0327805e89d4a21c66e9c88b51240879d0ad07
Isaac Dyor <isaac@dyor.com>
2026-02-25 13:33:29 -0800

    update vite config

--- apps/server/src/db/migrations/migrations.js
+++ apps/server/src/db/migrations/migrations.js
@@ -1,16 +1,15 @@
-import journal from './meta/_journal.json';
-import m0000 from './0000_glorious_darwin.sql';
-import m0001 from './0001_careful_harrier.sql';
-import m0002 from './0002_sturdy_falcon.sql';
-import m0003 from './0003_robust_captain_midlands.sql';
+import m0000 from "./0000_glorious_darwin.sql";
+import m0001 from "./0001_careful_harrier.sql";
+import m0002 from "./0002_sturdy_falcon.sql";
+import m0003 from "./0003_robust_captain_midlands.sql";
+import journal from "./meta/_journal.json";
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-m0001,
-m0002,
-m0003
-    }
-  }
-  
+export default {
+	journal,
+	migrations: {
+		m0000,
+		m0001,
+		m0002,
+		m0003,
+	},
+};
